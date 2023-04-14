f6b35dcd0cb7b4280cba75fc3bd88e210699f07c
contactamol <70578572+contactamol@users.noreply.github.com>
2023-04-14 12:30:34 +0530

    Update test_api_response.py

--- test_api_response.py
+++ test_api_response.py
@@ -1,18 +1,25 @@
 import requests
 import pytest
 
+# API URL to be Tested
 API_URL = "https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals"
+
+# fixture that will return instance of requests and session
 
 
 @pytest.fixture
 def api_client():
     return requests.Session()
 
+# function to check response code
+
 
 def test_api_response_code(api_client):
     response = api_client.get(API_URL)
 
     assert response.status_code == 200
+
+# function to check null values if present in 'name' and 'bands'
 
 
 def test_api_null_values(api_client):
@@ -23,9 +30,15 @@ def test_api_null_values(api_client):
         if isinstance(data, dict):
             assert data["name"] is not None
             assert data["bands"] is not None
+            for band in data["bands"]:
+                assert band["name"] is not None
+                assert band["recordLabel"] is not None
         elif isinstance(data, list):
-            for item in data:
-                assert item["name"] is not None
-                assert item["bands"] is not None
+            for festival in data:
+                assert festival["name"] is not None
+                assert festival["bands"] is not None
+                for band in festival["bands"]:
+                    assert band["name"] is not None
+                    assert band["recordLabel"] is not None
     except KeyError:
         pytest.fail("Expected keys not found")
